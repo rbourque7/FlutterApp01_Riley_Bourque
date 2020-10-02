80729282fd35f458f1c0c9bc9b6b7cd3dc52f63a
rbourque7 <59621985+rbourque7@users.noreply.github.com>
2020-10-02 17:08:33 -0600

    containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,80 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
- build(context) {
+  @override
+ Widget build(BuildContext context) {
    return MaterialApp(
      home: Scaffold(
        backgroundColor: Colors.blueGrey,
        appBar: AppBar(
          title: Text('Row and Column Widgets'),
          ),
+         body: Row(
+           children: [
+             Column(
+               children: [
+                 Container(
+	                child: Text('Container 1'),
+	                decoration: BoxDecoration(		                
+		                color: Colors.yellow,
+                	),
+	                margin: EdgeInsets.all(10.0),
+	                padding: EdgeInsets.all(10.0)             
+                ),
+                  Container(
+	                child: Text('Container 2'),
+	                decoration: BoxDecoration(		                
+		                color: Colors.white,
+                	),
+	                margin: EdgeInsets.all(10.0),
+	                padding: EdgeInsets.all(10.0),
+                  transform: Matrix4.rotationZ(0.05),            
+                ),             
+               ],
+              ),
+             Column(
+               children: [
+                 Container(
+	                child: Text('Container 1'),
+	                decoration: BoxDecoration(		                
+		                color: Colors.yellowAccent,
+                	),
+	                margin: EdgeInsets.all(10.0),
+	                padding: EdgeInsets.all(10.0)             
+                ),
+                  Container(
+	                child: Text('Container 2'),
+	                decoration: BoxDecoration(		                
+		                color: Colors.white,
+                	),
+	                margin: EdgeInsets.all(10.0),
+	                padding: EdgeInsets.all(10.0),                          
+                ),
+                
+               ],),
+             Column(
+               children: [
+                 Container(
+	                child: Text('Container 1'),
+	                decoration: BoxDecoration(		                
+		                color: Colors.yellow,
+                	),
+	                margin: EdgeInsets.all(10.0),
+	                padding: EdgeInsets.all(10.0)             
+                ),
+                  Container(
+	                child: Text('Container 2'),
+	                decoration: BoxDecoration(		                
+		                color: Colors.white,
+                	),
+	                margin: EdgeInsets.all(10.0),
+	                padding: EdgeInsets.all(10.0),                              
+                ),
+               ],)
+           ],
+         )
        ),
      );
    }
